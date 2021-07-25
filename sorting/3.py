f96500d5c3fce3fd408a0d22028f56b057f132f7
kimook <garam.lim@gooroomee.com>
2021-07-25 23:27:33 +0900

    수정

--- sorting/3.py
+++ sorting/3.py
@@ -1,9 +1,9 @@
 def solution(citations):
     citations.sort(reverse= True)
-    temp = None
+    temp = 0
     for i, val in enumerate(citations, 1):
         if i <= val: temp = i
     return temp
 
 if __name__ == "__main__":
-	print(solution([1]))
+	print(solution([0,0,0]))
