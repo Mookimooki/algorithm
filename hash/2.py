f2cad63fe44028e349d4968be25ea5f10ec0e6af
kimook <garam.lim@gooroomee.com>
2021-07-31 20:00:38 +0900

    성공한 버전

--- hash/2.py
+++ hash/2.py
@@ -1,9 +1,14 @@
+import collections
+
 def solution(phone_book):
-    phone_book.sort(key = len)
-    for i, number in enumerate(phone_book):
-        if number in list(map(lambda x: x[:len(number)], phone_book[i+1:])):
-           return False
-    return True
+    temp = {}
+    for length in set(map(len, phone_book)):
+       temp[length] = collections.Counter(map(lambda x: x[:length], phone_book))
+       
+    for num in phone_book:
+        if temp[len(num)][num] > 1: return False
+    else: 
+        return True
 
 
 print(solution(["123","456","789"]))
