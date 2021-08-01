c5b0e168f659c0249c14bcb7642cac893ec559f7
kimook <garam.lim@gooroomee.com>
2021-08-02 00:59:06 +0900

    완전한 버전

--- queue/stack/2.py
+++ queue/stack/2.py
@@ -1,19 +1,14 @@
 def solution(priorities, location):
     answer = 1
-    mine = priorities[location]
     temp = [x for x in enumerate(priorities)]    
-    
-    biggerThanMine = [x for x in temp if x[1] > mine]                      
-    if len(biggerThanMine) > 0:
-        minAmongBigger = min(biggerThanMine, key = lambda x: (x[1], -x[0]))
-        answer += len(biggerThanMine)
-        temp = temp[minAmongBigger[0]:] + temp[:minAmongBigger[0]]
-        
-    temp = [x for x in temp if x[1] ==  mine]
-    for x in temp:
-        if x[0] == location:
-            break;
+
+    while len(temp) > 1:
+        maxTuple = max(enumerate(temp), key = lambda x : x[1][1])
+        if maxTuple[1][0] == location:
+            break
         answer += 1
+        temp = temp[maxTuple[0]+1:] + temp[:maxTuple[0]]        
+        
     return answer
 
-print(solution([1,2,9,1,2,2], 3))
+print(solution([1,2,9,9,1,2,2], 4))
