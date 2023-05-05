1e1aed7a12a5467454b674ecfb1ce5cf56efc9c5
kimook <garam.lim@gooroomee.com>
2023-05-05 23:19:36 +0900

    그리디 5번 문제 초기화

---
+++ greedy/5.py
@@ -0,0 +1,11 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/42861
+
+def solution(n, costs):
+    answer = 0
+    visited = [False for i in range(n)]
+    
+    
+    return answer
+
+result = solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])
+print(result, result == 4)
