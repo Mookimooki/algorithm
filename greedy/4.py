ffccdd7883fe13d21d54804eaaccd60f3a7ee6dd
macbook-garam <rlanr5812@naver.com>
2023-05-05 13:55:03 +0900

    효율성 개선

--- greedy/4.py
+++ greedy/4.py
@@ -1,6 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42885
 
-def solution(people, limit):
+def solution1(people, limit):
     people.sort()
     answer = 0
     weightSum = 0
@@ -20,6 +20,27 @@ def solution(people, limit):
     
     return answer
 
+def solution(people, limit):
+    people.sort(reverse = True)
+    answer = 0
+    
+    while len(people) > 0:
+        minWeight = people.pop()
+        weightSum = minWeight
+        answer += 1
+
+        while limit >= weightSum + minWeight and len(people) > 0:
+            weight = people.pop(0)
+            if weightSum + weight > limit:
+                answer += 1
+            else:
+                weightSum += weight
+                if limit < minWeight + weightSum:
+                    weightSum = 0
+                    break
+    
+    return answer
+
 print(solution([70, 50, 80, 50], 100))  #3
 print(solution([70, 80, 50], 100))      #3
-print(solution([50, 100, 100, 140], 200))      #2
+# print(solution([50, 100, 100, 140, 180], 200))      #3
