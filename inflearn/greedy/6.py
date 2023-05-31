84d7bd980345a0d4ee5e2830b565ebed27dec261
kimook <garam.lim@gooroomee.com>
2023-05-31 23:30:58 +0900

    최종

--- inflearn/greedy/6.py
+++ inflearn/greedy/6.py
@@ -1,18 +1,17 @@
 #https://cote.inflearn.com/contest/10/problem/09-06
 
-# n, numOfPairs = map(int, input().split())
-# friends = []
+n, numOfPairs = map(int, input().split())
+friends = []
 
-# for m in range(n):
-#     input1, input2 = map(int, input().split())
-#     friends.append([input1, input2])
+for m in range(numOfPairs+1):
+    input1, input2 = map(int, input().split())
+    friends.append([input1, input2])
 
-
-n = 9
-numOfPairs = 7
-friends = [
-    [1, 2],[2, 3],[3, 4],[1, 5],[6, 7],[7, 8],[8, 9],[3, 8]
-]
+# n = 9
+# numOfPairs = 7
+# friends = [
+#     [1, 2],[2, 3],[3, 4],[1, 5],[6, 7],[7, 8],[8, 9],[3, 8]
+# ]
 unf = [x for x in range(n+1)]
 
 def find(a):
