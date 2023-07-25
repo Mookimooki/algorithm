a025f4632c0a6e0e62a462da13a323b9110921c8
kimook <garam.lim@gooroomee.com>
2023-07-26 00:08:43 +0900

    행복도 문제 풀이

--- oHouse/5.py
+++ oHouse/5.py
@@ -1,28 +1,20 @@
 def solution(happiness):
-    dp = [True if x > 8 else False for x in happiness]
+    happiness = [1 if x > 8 else -1 for x in happiness]
+    length = len(happiness)
     
-    maxCnt = 0
-    happyCnt = 0
-    wholeCnt = 0
+    dp = [[0 for _ in range(length)] for _ in range(length)]
     
-    for idx in range(len(dp)):
-        happyDay = dp[idx]
-        if happyDay:
-            happyCnt += 1
-        wholeCnt += 1
-        
-        if happyCnt > wholeCnt / 2:
-            maxCnt = max(maxCnt, wholeCnt)
-        else:
-            temp = idx -1
-            while(wholeCnt > 1):
-                wholeCnt -= 1
-                dp[temp] = 0
-                temp -= 1
-            wholeCnt = 0
-            happyCnt = 0
-        
-    return maxCnt
+    maxDays = 0
+    for i in range(length):
+        for j in range(i, length):
+            if i == j: dp[i][i] = happiness[i] 
+            else: dp[i][j] = dp[i][j-1] + happiness[j]
+            
+            if dp[i][j] >= 1:
+                maxDays = max(maxDays, j - i + 1)
 
-# print(solution([9, 10, 6, 0, 4, 6, 10]))
-print(solution([6, 10, 3, 9, 4, 10, 3, 9, 3, 10, 6]))
+    return maxDays
+
+print(solution([9, 10, 6, 0, 4, 6, 10]), 3) 
+print(solution([6, 10, 3, 9, 4, 10, 3, 9, 3, 10, 6]), 9)
+print(solution([5, 3, 1, 3, 6, 4]), 0)
