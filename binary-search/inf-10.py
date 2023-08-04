08f6c47fb7a3876000f6287ed653a327ea8425b9
kimook <garam.lim@gooroomee.com>
2023-08-04 10:17:33 +0900

    마구간 정하기 문제풀이

--- binary-search/inf-10.py
+++ binary-search/inf-10.py
@@ -1,5 +1,5 @@
 def distance(c, stables, minimum):
-    prev = 0
+    prev = stables[0]
     c -= 1
     for i in range(1, len(stables)):
         if stables[i] - prev >= minimum: 
@@ -8,11 +8,14 @@ def distance(c, stables, minimum):
             if c == 0: return minimum
     return minimum - 1
 
-n, c = map(int, input().split())
-stables = list(map(int, input().split()))
+# n, c = map(int, input().split())
+# stables = list(map(int, input().split()))
+
+n, c = 5, 3
+stables = [1, 2, 8, 4, 9]
 
 stables.sort()
-lt, rt = 1, max(stables)
+lt, rt = 1, stables[-1]
 
 answer = 0
 while lt <= rt:
