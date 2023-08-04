9d7c2acc7e8481c0a1fc5f5d07c25040e8c568ab
macbook-garam <rlanr5812@naver.com>
2023-08-04 18:11:09 +0900

    입국심사 문제 풀이

--- binary-search/1.py
+++ binary-search/1.py
@@ -1,12 +1,19 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/43238
 
-def solution(n, times, answer):   
-    i, mins = 1, 0
-    length = len(times)
+def check(times, answer):
+    return sum(map(lambda x: answer // x, times))
+
+def solution(n, times):   
+    lt, rt = 1, max(times) * n // len(times)
     
-    while mins < answer:
-        mins += times[i]
-        
+    answer = 0
+    while lt <= rt:
+        mid = (lt + rt)//2
+        if check(times, mid) >= n:
+            answer = mid
+            rt = mid - 1
+        else:
+            lt = mid + 1
     return answer
 
 print(solution(6, [7, 10]), 28)
