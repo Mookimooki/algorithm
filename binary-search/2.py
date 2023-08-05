08f448dad8c3d163d2402a7e9ef220a9a2b83dd0
kimook <garam.lim@gooroomee.com>
2023-08-05 14:09:03 +0900

    징검다리 문제 풀이 - 시간초과

---
+++ binary-search/2.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+
+def findMinDistance(way):
+    answer = way[-1]
+    for i in range(1, len(way)):
+        answer = min(answer , way[i] - way[i-1])
+    return answer
+
+def solution(distance, rocks, n):
+    rocks.sort()
+    lt, rt = 1, distance - rocks[-1]
+    
+    ways = tuple(map(lambda x: (0,) + x + (distance,), combinations(rocks, len(rocks) - n)))
+    # answer = 0
+    # while lt <= rt:
+    #     mid = (lt + rt)//2
+    #     if max(map(findMinDistance, ways)) >= mid:
+    #         answer = mid
+    #         rt = mid - 1
+    #     else:
+    #         lt = mid + 1
+    # return answer
+    return max(map(findMinDistance, ways))
+
+print(solution(25, [2, 14, 11, 21, 17], 2))
