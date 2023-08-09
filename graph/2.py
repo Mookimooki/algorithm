d75b5d5ae8969069ca24014e0ccfde5e85a03d1a
kimook <garam.lim@gooroomee.com>
2023-08-09 14:06:26 +0900

    인접 리스트로 풀이 - 오답

--- graph/2.py
+++ graph/2.py
@@ -1,26 +1,22 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/49191
 
 def solution(n, results):
-    graph = [[] for _ in range(n+1)]
+    graph, first = [[] for _ in range(n+1)], [x+1 for x in range(n)]
     for a, b in results:
-        graph[b].append(a)
-            
-    
-    q, rank, first = [0], [set() for _ in range(n+1)], set()
-    def find(n):
-        if len(graph[n]) < 1: 
-            first.add(n)
-            return {n}
+        graph[a].append(b)
+        if b in first: first.remove(b)
         
-        if len(rank[n]) < 1:
-            for winner in graph[n]:
-                rank[n].update(find(winner))
-        return rank[n]
+    for i in first:
+        if len(graph[i]) == 0: graph[0].append(i)
         
-    for i in range(1, n+1):
-        find(i)
+    q, rank = [0], [0 for _ in range(n+1)]
+    while len(q) > 0:
+        winner = q.pop(0)
+        for loser in graph[winner]:
+            rank[loser] = rank[winner] + 1
+            q.append(loser)
         
-    return sum(map(lambda x: 1 if x == first else 0, rank))
-
+    answer = 0
+    return answer
 
 print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]), 2)
