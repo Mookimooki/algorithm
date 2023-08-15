2f1de6ea93a2b1ad697627c0067640979569047d
kimook <garam.lim@gooroomee.com>
2023-08-15 17:38:46 +0900

    1번문제 풀이

--- channel/1.py
+++ channel/1.py
@@ -2,9 +2,7 @@ import math
 
 def solution(mod1, mod2, max_range):
     return max_range // mod1 - max_range // ((mod1 * mod2) // math.gcd(mod1, mod2))
-    # for i in range(mod1, max_range + 1, mod1):
-    #     if i % mod2 != 0: answer += 1
-    # return answer
+
 
 print(solution(4, 4, 20), 0)
 print(solution(4, 3, 20), 4)
