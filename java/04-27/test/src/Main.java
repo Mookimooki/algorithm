5ded18d2fcf6a3652d75f1f3f39825208eba4dc5
garam <rlanr5812@naver.com>
2024-04-27 23:32:18 +0900

    added for inflearn6

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,8 +1,15 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    private String solution(String string){
+        String answer = "";
+        Set<String> stringSet = new HashSet<>();
+        for(char ch: string.toCharArray()){
+            if(!stringSet.contains(String.valueOf(ch))) answer += ch;
+            stringSet.add(String.valueOf(ch));
+        }
 
         return answer;
     }
@@ -10,8 +17,7 @@ public class Main {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
-        char c = scanner.next().charAt(0);
 
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(input));
     }
 }
