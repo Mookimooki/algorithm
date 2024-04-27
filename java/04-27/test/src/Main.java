af82a775f5746087df7dcb757df4de8c8924a026
garam <rlanr5812@naver.com>
2024-04-27 17:34:52 +0900

    init

---
+++ java/04-27/test/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    private int solution1(String string, char c){
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        char c = scanner.next().charAt(0);
+
+        System.out.println(main.solution1(input, c));
+    }
+}
