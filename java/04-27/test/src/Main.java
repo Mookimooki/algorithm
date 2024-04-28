5fbe32c34d7f50f243aa190adf86bf92eff09d2b
garam <rlanr5812@naver.com>
2024-04-28 23:57:17 +0900

    added for inflearn2-2

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    private int solution(int[] input){
+        int answer = 1;
+        int maxHeight = input[0];
+
+        for (int i = 1; i < input.length; i++) {
+            if(input[i] > maxHeight) {
+                answer++;
+                maxHeight = input[i];
+            }
+        }
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int length = scanner.nextInt();
+        int[] input = new int[length];
+        for (int i = 0; i < length; i++) {
+            input[i] = scanner.nextInt();
+        }
 
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(input));
     }
 }
