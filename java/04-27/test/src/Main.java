416542994094d6c4664d813fc4d845c1b19d935c
garam <rlanr5812@naver.com>
2024-04-29 23:34:49 +0900

    added for inflearn2-5

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
+    private int solution(int input){
         int answer = 0;
+        boolean[] numbsers = new boolean[input+1];
+
+        for (int i = 2; i < input+1; i++) {
+            if(!numbsers[i]){
+                answer++;
+                for (int j = i; j < input+1; j+=i) {
+                    numbsers[j] = true;
+                }
+            }
+        }
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int input = scanner.nextInt();
 
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(input));
     }
 }
