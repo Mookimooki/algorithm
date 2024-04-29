d24497e0ddf657df33286a8f839e6a7c29ec0b28
garam <rlanr5812@naver.com>
2024-04-29 23:45:42 +0900

    added for inflearn2-6

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    private List<Integer> solution(int[] input){
+        List<Integer> answer = new ArrayList<>();
+        int max = Arrays.stream(input).max().getAsInt();
+        boolean[] numbers = new boolean[max+1];
+
+        numbers[0] = numbers[1] = true;
+        for (int i = 2; i < max+1; i++) {
+            if(!numbers[i]){
+                for (int j = i*2; j < max+1; j+=i) {
+                    numbers[j] = true;
+                }
+            }
+        }
+
+        for(int number: input){
+            if (!numbers[number]) answer.add(number);
+        }
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int count = scanner.nextInt();
+        int[] input = new int[count];
 
-        System.out.println(main.solution(input, c));
+        for (int i = 0; i < count; i++) {
+            String str = scanner.next();
+            input[i] = Integer.parseInt(new StringBuilder(str).reverse().toString());
+        }
+
+        for (int ans: main.solution(input))
+            System.out.print(ans + " ");
     }
 }
