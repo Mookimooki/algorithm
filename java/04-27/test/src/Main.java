bd6c04afa90f7bb8c8bf892bd3d6d3867ad44496
garam <rlanr5812@naver.com>
2024-04-29 23:16:07 +0900

    added for inflearn2-3

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    private char[] solution(int[][] input){
+        char[] answer = new char[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            if(input[i][0] == input[i][1]) answer[i] = 'D';
+            else if(input[i][0] == 1 && input[i][1] == 3) answer[i] = 'A';
+            else if(input[i][0] == 3 && input[i][1] == 1) answer[i] = 'B';
+            else if(input[i][0] < input[i][1]) answer[i] = 'B';
+            else answer[i] = 'A';
+        }
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int count = scanner.nextInt();
+        int[][] input = new int[count][2];
 
-        System.out.println(main.solution(input, c));
+        for (int i = 0; i < 2; i++) {
+            for (int k = 0; k < count; k++) {
+                input[k][i] = scanner.nextInt();
+            }
+        }
+
+        for(char ch: main.solution(input))
+            System.out.println(ch);
     }
 }
