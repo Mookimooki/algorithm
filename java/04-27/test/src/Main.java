915b364f6fa055a19184f2146c904b7c815ba10c
macbook-garam <rlanr5812@naver.com>
2024-05-01 22:45:39 +0900

    added for inflearn 2-8

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    private int[] solution(int[] input){
+        int[] answer = new int[input.length];
+
+        Arrays.sort(input);
+
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
-
-        System.out.println(main.solution(input, c));
+        int count = scanner.nextInt();
+        int[] input = new int[count];
+        for (int i = 0; i < count; i++) {
+            input[i] = scanner.nextInt();
+        }
+        for (int score: main.solution(input)) {
+            System.out.print(main.solution(input) + " ");
+        }
     }
 }
