b7f9f180d5b5442215234f7d8a6a511cd4bc6386
macbook-garam <rlanr5812@naver.com>
2024-05-09 20:09:52 +0900

    added for inflearn 2-8

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -5,9 +5,15 @@ public class Main {
     private int[] solution(int[] input){
         int[] answer = new int[input.length];
 
-        Arrays.sort(input);
-
-
+        for(int i = 0; i < input.length; i++){
+            int rank = 1;
+            for(int j = 0; j < input.length; j++){
+                if(input[j] > input[i]){
+                    rank++;
+                }
+            }
+            answer[i] = rank;
+        }
         return answer;
     }
     public static void main(String[] args) {
@@ -19,7 +25,7 @@ public class Main {
             input[i] = scanner.nextInt();
         }
         for (int score: main.solution(input)) {
-            System.out.print(main.solution(input) + " ");
+            System.out.print(score + " ");
         }
     }
 }
