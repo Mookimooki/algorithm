9dd1ea7010159a0879b318da766c5e377e041c72
macbook-garam <rlanr5812@naver.com>
2024-05-09 20:56:00 +0900

    leetcode init

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
+    private int solution(String[] string){
         int answer = 0;
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        String[] input = {"3[a2[c]]", "3[a2[c]]"};
 
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(input));
     }
 }
