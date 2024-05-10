86ea1a13776b9326048981e805d6550d6ee14e64
macbook-garam <rlanr5812@naver.com>
2024-05-11 00:38:33 +0900

    solved

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,15 +1,53 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    private int solution(String[] string){
-        int answer = 0;
-
-        return answer;
-    }
     public static void main(String[] args) {
         Main main = new Main();
-        String[] input = {"3[a2[c]]", "3[a2[c]]"};
+        List matrix = new ArrayList<List<Integer>>() {{
+            add(new ArrayList<Integer>() {{
+                add(112);
+                add(42);
+                add(83);
+                add(119);
+            }});
+            add(new ArrayList<Integer>() {{
+                add(56);
+                add(125);
+                add(56);
+                add(49);
+            }});
+            add(new ArrayList<Integer>() {{
+                add(15);
+                add(78);
+                add(101);
+                add(43);
+            }});
+            add(new ArrayList<Integer>() {{
+                add(62);
+                add(98);
+                add(114);
+                add(108);
+            }});
+        }};
+        main.flippingMatrix(matrix);
+    }
 
-        System.out.println(main.solution(input));
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+        int n = matrix.size() / 2;
+        int size = matrix.size();
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                answer += Arrays.stream(new int[]{
+                        matrix.get(i).get(j),
+                        matrix.get(i).get(size - j - 1),
+                        matrix.get(size - i - 1).get(j),
+                        matrix.get(size - i - 1).get(size - j - 1)
+                }).max().getAsInt();
+            }
+        }
+        return answer;
     }
 }
