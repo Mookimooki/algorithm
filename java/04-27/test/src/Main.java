549babd9ee8df0a5f9e1293785cd22ffc71a0333
macbook-garam <rlanr5812@naver.com>
2024-05-11 01:05:27 +0900

    solved

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,15 +1,37 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    private int solution(String[] string){
-        int answer = 0;
-
-        return answer;
-    }
     public static void main(String[] args) {
         Main main = new Main();
-        String[] input = {"3[a2[c]]", "3[a2[c]]"};
 
-        System.out.println(main.solution(input));
+        for (Integer answer: main.countingSort(new ArrayList<>() {{
+            add(1);
+            add(1);
+            add(1);
+            add(63);
+            add(25);
+            add(73);
+            add(98);
+            add(73);
+            add(56);
+            add(84);
+            add(86);
+            add(57);
+            add(16);
+            add(83);
+            add(8);
+        }}))
+            System.out.print(answer + " ");
+    }
+
+    public List<Integer> countingSort(List<Integer> arr) {
+        List<Integer> count = new ArrayList<>(Collections.nCopies(100, 0));
+
+        for (int i = 0; i < arr.size(); i++) {
+            count.set(arr.get(i), count.get(arr.get(i)) + 1);
+        }
+        return count;
     }
 }
