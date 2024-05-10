58334f16cf39e5a498f85f7fd7999261bfae6bf6
macbook-garam <rlanr5812@naver.com>
2024-05-10 22:01:05 +0900

    leetcode init

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String[] string){
-        int answer = 0;
+    private int solution(int n){
+        if(n < 3) return n;
+        int[] stairs = new int[n+1];
+        stairs[1] = 1;
+        stairs[2] = 2;
 
-        return answer;
+        for (int i = 3; i <= n; i++) {
+            stairs[i] = stairs[i-1] + stairs[i-2];
+        }
+        return stairs[n];
     }
     public static void main(String[] args) {
         Main main = new Main();
-        String[] input = {"3[a2[c]]", "3[a2[c]]"};
 
-        System.out.println(main.solution(input));
+        System.out.println(main.solution(1));
     }
 }
