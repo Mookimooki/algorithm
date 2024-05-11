be47c142a361d5ccfcb081f03c6442634e288055
macbook-garam <rlanr5812@naver.com>
2024-05-12 00:09:31 +0900

    solved by dfs

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,40 @@
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    private int solution(String string, char c){
+    public int solution(int n, int[][] computers) {
         int answer = 0;
+        int[] visited = new int[n];
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < n; i++) computers[i][i] = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == 0) {
+                visited[i] = 1;
+                answer++;
+
+                queue.offer(i);
+                while(!queue.isEmpty()) {
+                    int x = queue.poll();
+                    for(int k = 0; k < n; k++) {
+                        if(computers[x][k] == 1) {
+                            visited[k] = 1;
+                            queue.offer(k);
+                            computers[x][k] = computers[k][x] = 0;
+                        }
+                    }
+                }
+            }
+        }
 
         return answer;
     }
+
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
 
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(3, new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+        System.out.println(main.solution(3, new int[][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
     }
 }
