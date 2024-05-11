a79c439eed39dc8935f6e3f2ef3603f6b8115717
macbook-garam <rlanr5812@naver.com>
2024-05-11 15:28:28 +0900

    solved

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     private int solution(String[] string){
@@ -8,8 +8,29 @@ public class Main {
     }
     public static void main(String[] args) {
         Main main = new Main();
-        String[] input = {"3[a2[c]]", "3[a2[c]]"};
+        main.findZigZagSequence(new int[]{1,2,3,4,5,6,7}, 7);
+    }
 
-        System.out.println(main.solution(input));
+    public static void findZigZagSequence(int [] a, int n){
+        Arrays.sort(a);
+        int mid = (n + 1)/2;
+        int temp = a[mid-1];
+        a[mid-1] = a[n - 1];
+        a[n - 1] = temp;
+
+        int st = mid;
+        int ed = n - 2;
+        while(st <= ed){
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1;
+        }
+        for(int i = 0; i < n; i++){
+            if(i > 0) System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
     }
 }
