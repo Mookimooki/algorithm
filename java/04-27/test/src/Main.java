f921d5d743eb50933abe0535da9866dc55389be7
macbook-garam <rlanr5812@naver.com>
2024-05-11 20:12:07 +0900

    solved by dfs

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,27 @@
-import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    static int n = 3, m = 2;
+    static int[] numbers = new int[] {3, 6, 9};
+    static int[] visited = new int[n];
 
-        return answer;
+    private void dfs(int length, Stack<Integer> arr, int maxNumber, int maxCount){
+        if(length == maxCount){
+            System.out.println(arr.stream().toList());
+            return;
+        }
+
+        for(int i = 0; i < maxNumber; i++){
+            if(visited[i] == 1) continue;
+            visited[i] = 1;
+            arr.push(numbers[i]);
+            dfs(length + 1, arr, maxNumber, maxCount);
+            arr.pop();
+            visited[i] = 0;
+        }
     }
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
-
-        System.out.println(main.solution(input, c));
+        main.dfs(0, new Stack<>(), n, m);
     }
 }
