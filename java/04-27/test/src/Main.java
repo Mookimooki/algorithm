c49e0f829707946d8580002b79f549850a0c769e
macbook-garam <rlanr5812@naver.com>
2024-05-12 00:33:32 +0900

    solved by dfs

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,41 @@
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    private int solution(String string, char c){
+    public int solution(int[][] maps) {
         int answer = 0;
+        int[][] directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
 
-        return answer;
+        Queue<int []> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            answer++;
+
+            for(int i = 0; i < size; i++) {
+                int[] point = queue.poll();
+                int x = point[0], y = point[1];
+                if(x == maps.length-1 && y == maps[0].length-1) return answer;
+                if(maps[x][y] == 0) continue;
+                maps[x][y] = 0;
+
+                for(int[] direction : directions) {
+                    int newX = x + direction[0], newY = y + direction[1];
+                    if(newX < 0 || newX >= maps.length || newY < 0 || newY >= maps[0].length || maps[newX][newY] == 0) continue;
+                    queue.add(new int[]{newX, newY});
+                }
+            }
+        }
+        return -1;
     }
+
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
-
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}}));
+        System.out.println(main.solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}}));
+        System.out.println(main.solution(new int[][]{{1, 1, 1, 1, 1},{1, 0, 1, 0, 1},{1, 1, 1, 1, 1},{1, 0, 1, 0, 1}, {1, 1, 1, 1, 1}}));
+        System.out.println(main.solution(new int[][]{{1, 1, 1, 1, 1},{1, 0, 0, 0, 1},{1, 0, 0, 0, 1},{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}}));
+        System.out.println(main.solution(new int[][]{{1, 0, 1, 1, 1},{0, 0, 1, 1, 1},{1, 1, 1, 1, 1},{1, 1, 1, 0, 0}, {1, 1, 1, 0, 1}}));
     }
 }
