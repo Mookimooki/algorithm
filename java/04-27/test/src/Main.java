aaee73a346dbd20918f7bb997bacf7541c46e3cc
macbook-garam <rlanr5812@naver.com>
2024-05-11 18:18:23 +0900

    solved

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,26 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
-
-        return answer;
+    static int sumOfArr;
+    static boolean flag = false;
+    private void bfs(int length, int sum, int[] arr){
+        if(flag) return;
+        if(sum > sumOfArr/2) return;
+        if(length == arr.length){
+            if (sum == sumOfArr/2) flag = true;
+            System.out.println(length + ": " + sum);
+            return;
+        }
+        bfs(length+1, sum+arr[length], arr);
+        bfs(length+1, sum, arr);
     }
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
 
-        System.out.println(main.solution(input, c));
+        int[] arr = new int[]{1, 3, 5, 6, 7, 10};
+        sumOfArr = Arrays.stream(arr).sum();
+        main.bfs(0, 0, arr);
+        if (flag) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
