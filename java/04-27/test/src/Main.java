ccd66bf0d678f0bcd64e492d5ad2485836d03990
macbook-garam <rlanr5812@naver.com>
2024-05-11 23:30:56 +0900

    solved by dfs

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    static int n, m;
+    static int[] combi;
+    private void dfs(int level, int number){
+        if(level == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(combi[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
 
-        return answer;
+        for (int i = number; i <= n; i++) {
+            combi[level] = i;
+            dfs(level + 1, i + 1);
+        }
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        combi = new int[m];
 
-        System.out.println(main.solution(input, c));
+        main.dfs(0, 1);
     }
 }
