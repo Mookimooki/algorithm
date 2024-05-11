b80678ca6e320c7b1a97067480dc3217227ced14
macbook-garam <rlanr5812@naver.com>
2024-05-11 15:39:34 +0900

    solved

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
-
-        return answer;
+    static int[] fibo;
+    private int solution(int input){
+        if(input < 3) return 1;
+        if(fibo[input] != 0) return fibo[input];
+        return fibo[input] = solution(input-1) + solution(input-2);
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int input = scanner.nextInt();
+        fibo = new int[input+1];
+        fibo[0] = fibo[1] = 1;
 
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(input));
     }
 }
