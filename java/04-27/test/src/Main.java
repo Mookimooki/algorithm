0ad258cd2cedf7ac1d21d7f811961c255054e5f3
macbook-garam <rlanr5812@naver.com>
2024-05-12 21:29:44 +0900

    solved by bfs

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,47 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    static int[][] directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
 
-        return answer;
-    }
     public static void main(String[] args) {
-        Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
 
-        System.out.println(main.solution(input, c));
+        int[][] grid = new int[7][7];
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                grid[i][j] = scanner.nextInt();
+            }
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        grid[0][0] = 1;
+
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            answer++;
+
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                int x = cur[0], y = cur[1];
+
+                for (int[] direction : directions) {
+                    int newX = x + direction[0], newY = y + direction[1];
+                    if(newX == 6 && newY == 6) {
+                        System.out.println(answer);
+                        return;
+                    }
+                    if(newX < 0 || newX >= 7 || newY < 0 || newY >= 7 || grid[newX][newY] == 1) continue;
+
+                    grid[newX][newY] = 1;
+                    queue.offer(new int[]{newX, newY});
+                }
+            }
+        }
+
+        System.out.println(-1);
     }
 }
