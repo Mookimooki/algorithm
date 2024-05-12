746af78e5e1be694c65ebc3272fa7180ed8437ba
macbook-garam <rlanr5812@naver.com>
2024-05-12 11:40:33 +0900

    solved by dfs

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,54 @@
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    private int solution(String string, char c){
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        final boolean[][] visited = new boolean[100][100];
         int answer = 0;
 
-        return answer;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{characterX, characterY});
+        visited[characterX][characterY] = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] point = queue.poll();
+                if (point[0] == itemX && point[1] == itemY)
+                    return answer;
+
+                for (int[] direction : directions) {
+                    int newX = point[0] + direction[0];
+                    int newY = point[1] + direction[1];
+
+                    if(newX < 0 || newY < 0 || visited[newX][newY]) continue;
+                    if(!isOnRectangle(rectangle, point[0] + (double) direction[0]/2,  point[1] + (double) direction[1]/2)) continue;
+                    queue.add(new int[]{newX, newY});
+                    visited[newX][newY] = true;
+                }
+            }
+            answer++;
+        }
+
+        return -1;
+    }
+    private boolean isOnRectangle(int[][] rectangle, double x, double y) {
+        boolean onRectangle = false;
+        for(int[] point : rectangle) {
+            int bottomLeftX = point[0], bottomLeftY = point[1], topRightX = point[2], topRightY = point[3];
+            if(x > bottomLeftX && topRightX > x && y > bottomLeftY && topRightY > y) return false;
+
+            if (x == bottomLeftX || x == topRightX) {
+                if (y >= bottomLeftY && y <= topRightY) onRectangle = true;
+            } else if (y == topRightY || y == bottomLeftY) {
+                if (x >= bottomLeftX && x <= topRightX) onRectangle = true;
+            }
+        }
+        return onRectangle;
     }
     public static void main(String[] args) {
         Main main = new Main();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
-
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(new int[][]{{1,1,7,4},{3,2,5,5},{4,3,6,9},{2,6,8,8}}, 1, 3, 7, 8));
     }
 }
