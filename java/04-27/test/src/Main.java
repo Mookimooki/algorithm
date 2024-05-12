2c268dda9bd2f2e69b6455114863c2097aff66e6
macbook-garam <rlanr5812@naver.com>
2024-05-12 22:02:04 +0900

    solved by bfs

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,61 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    public int bfs(int[][] tomatoes){
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i = 0; i < tomatoes.length; i++){
+            for(int j = 0; j < tomatoes[i].length; j++){
+                if(tomatoes[i][j] == 1){
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+
+        if(queue.isEmpty()) return 0;
+
+        int answer = -1;
+        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        while(!queue.isEmpty()){
+            answer ++;
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int[] arr = queue.poll();
+                int x = arr[0], y = arr[1];
+                for(int[] direction : directions){
+                    int newX = x + direction[0], newY = y + direction[1];
+                    if (newX < 0 || newX >= tomatoes.length || newY < 0 || newY >= tomatoes[0].length || tomatoes[newX][newY] != 0) continue;
+                    tomatoes[newX][newY] = 1;
+                    queue.add(new int[]{newX, newY});
+                }
+            }
+        }
+
+        for(int i = 0; i < tomatoes.length; i++){
+            for(int j = 0; j < tomatoes[i].length; j++){
+                if(tomatoes[i][j] == 0){
+                    return -1;
+                }
+            }
+        }
+
 
         return answer;
     }
+
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[][] tomatoes = new int[n][m];
 
-        System.out.println(main.solution(input, c));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                tomatoes[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(main.bfs(tomatoes));
     }
 }
