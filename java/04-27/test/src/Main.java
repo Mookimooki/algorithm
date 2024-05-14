a30ec043c934368da17da1e422e820c7e994d347
macbook-garam <rlanr5812@naver.com>
2024-05-14 18:49:08 +0900

    solved by array

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,61 @@
-import java.util.Scanner;
+import java.util.*;
+
+class Edge implements Comparable<Edge> {
+    int dest;
+    int cost;
+
+    Edge(int dest, int cost) {
+        this.dest = dest;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return Integer.compare(this.cost, o.cost);
+    }
+}
 
 public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
+    private int[] solution(int[][] graph, int n, int start){
+        int[] distance = new int[n+1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
 
-        return answer;
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+        distance[start] = 0;
+
+        while(!pq.isEmpty()) {
+            Edge curr = pq.poll();
+            int currentDestination = curr.dest;
+            int currentCost = curr.cost;
+
+            if(distance[currentDestination] < currentCost) continue;
+            for(int i = 1; i <= n; i++){
+                if(i == start || graph[currentDestination][i] == -1) continue;
+
+                if(distance[i] > currentCost + graph[currentDestination][i]){
+                    distance[i] = currentCost + graph[currentDestination][i];
+                    pq.offer(new Edge(i, distance[i]));
+                }
+            }
+        }
+
+        return distance;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
 
-        System.out.println(main.solution(input, c));
+        int[][] graph = new int[n+1][n+1];
+        for(int i = 0; i <= n; i++) {
+            Arrays.fill(graph[i], -1);
+        }
+        for(int i = 0; i < m; i++){
+            graph[scanner.nextInt()][scanner.nextInt()] = scanner.nextInt();
+        }
+
+        main.solution(graph, n, 1);
     }
 }
