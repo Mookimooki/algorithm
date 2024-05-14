8d73a77d02107808b6de634d2ef0552578e8e175
macbook-garam <rlanr5812@naver.com>
2024-05-14 20:19:28 +0900

    solved by pq

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,40 @@
 import java.util.Scanner;
 
-public class Main {
-    private int solution(String string, char c){
-        int answer = 0;
-
-        return answer;
+public class Main {;
+    static int[] findAndUnion;
+    public int find(int value){
+        if (findAndUnion[value] == value) return value;
+        return findAndUnion[value] = find(findAndUnion[value]);
     }
+    public void union(int a, int b){
+        int findA = find(a);
+        int findB = find(b);
+        findAndUnion[findA] = findB;
+    }
+
+
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
 
-        System.out.println(main.solution(input, c));
+        findAndUnion = new int[n+1];
+        for (int i = 1; i <= n; i++) findAndUnion[i] = i;
+
+        int[][] input = new int[m+1][2];
+        for (int i = 1; i <= m; i++) {
+            input[i][0] = scanner.nextInt();
+            input[i][1] = scanner.nextInt();
+        }
+
+        for (int i = 1; i <= m; i++) {
+            main.union(input[i][0], input[i][1]);
+        }
+
+        if(main.find(scanner.nextInt()) == main.find(scanner.nextInt()))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
     }
 }
