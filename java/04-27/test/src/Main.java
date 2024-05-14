c0cd4d547a850dc94e925a845f7e0617f97205ba
macbook-garam <rlanr5812@naver.com>
2024-05-14 18:57:45 +0900

    solved by pq

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -16,7 +16,7 @@ class Edge implements Comparable<Edge> {
 }
 
 public class Main {
-    private int[] solution(int[][] graph, int n, int start){
+    private int[] solution(List<List<Edge>> graph, int n, int start){
         int[] distance = new int[n+1];
         Arrays.fill(distance, Integer.MAX_VALUE);
 
@@ -30,13 +30,11 @@ public class Main {
             int currentCost = curr.cost;
 
             if(distance[currentDestination] < currentCost) continue;
-            for(int i = 1; i <= n; i++){
-                if(i == start || graph[currentDestination][i] == -1) continue;
+            for(Edge edge: graph.get(currentDestination)){
+                if(distance[edge.dest] <= currentCost + edge.cost) continue;
 
-                if(distance[i] > currentCost + graph[currentDestination][i]){
-                    distance[i] = currentCost + graph[currentDestination][i];
-                    pq.offer(new Edge(i, distance[i]));
-                }
+                distance[edge.dest] = currentCost + edge.cost;
+                pq.offer(new Edge(edge.dest, currentCost + edge.cost));
             }
         }
 
@@ -48,12 +46,12 @@ public class Main {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
-        int[][] graph = new int[n+1][n+1];
+        List<List<Edge>> graph = new ArrayList<>();
         for(int i = 0; i <= n; i++) {
-            Arrays.fill(graph[i], -1);
+            graph.add(new ArrayList<>());
         }
         for(int i = 0; i < m; i++){
-            graph[scanner.nextInt()][scanner.nextInt()] = scanner.nextInt();
+            graph.get(scanner.nextInt()).add(new Edge(scanner.nextInt(), scanner.nextInt()));
         }
 
         main.solution(graph, n, 1);
