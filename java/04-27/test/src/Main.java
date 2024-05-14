10b88b6945a0ea4c06656da05fd9fb0d1858c70d
macbook-garam <rlanr5812@naver.com>
2024-05-14 23:58:07 +0900

    solved

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,48 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
+    private int count(int[] barns, int distance){
+        int answer = 1, sum = 0;
+
+        for(int i = 0; i < barns.length-1; i++){
+            sum += barns[i+1] - barns[i];
+            if(sum >= distance){
+                sum = 0;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+    private int solution(int[] barns, int horseCount){
         int answer = 0;
+
+        int lt = 1;
+        int rt = barns[barns.length - 1];
+        int mid;
+        while (lt <= rt) {
+            mid = (lt + rt) / 2;
+            if(count(barns, mid) >= horseCount){
+                answer = mid;
+                lt = mid + 1;
+            }else {
+                rt = mid - 1;
+            }
+        }
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
+        int n = scanner.nextInt(), c = scanner.nextInt();
+        int[] barns = new int[n];
+        for(int i = 0; i < n; i++){
+            barns[i] = scanner.nextInt();
+        }
+        Arrays.sort(barns);
 
-        System.out.println(main.solution(input, c));
+        System.out.println(main.solution(barns, c));
     }
 }
