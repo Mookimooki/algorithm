91231fdb5af570593696752cfc8b72585d4f76d0
macbook-garam <rlanr5812@naver.com>
2024-05-14 20:31:41 +0900

    solved by pq

--- java/04-27/test/src/Main.java
+++ java/04-27/test/src/Main.java
@@ -1,17 +1,48 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-    private int solution(String string, char c){
+    int[] findAndUnion;
+    public int find(int value){
+        if(findAndUnion[value] != value){
+            return findAndUnion[value] = find(findAndUnion[value]);
+        }else return value;
+    }
+    public void union(int a, int b){
+        int findA = find(a);
+        int findB = find(b);
+        findAndUnion[findA] = findB;
+    }
+    private int solution(int[][] graph, int length){
         int answer = 0;
+
+        Arrays.sort(graph, Comparator.comparingInt(o -> o[2]));
+        findAndUnion = new int[length+1];
+        for(int i = 1; i <= length; i++) findAndUnion[i] = i;
+
+        for(int[] edge : graph){
+            if(find(edge[0]) == find(edge[1])) continue;
+            union(edge[0], edge[1]);
+            answer += edge[2];
+        }
 
         return answer;
     }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char c = scanner.next().charAt(0);
 
-        System.out.println(main.solution(input, c));
+        int v = scanner.nextInt();
+        int e = scanner.nextInt();
+
+        int[][] graph = new int[e][3];
+        for(int i = 0; i < e; i++) {
+            graph[i][0] = scanner.nextInt();
+            graph[i][1] = scanner.nextInt();
+            graph[i][2] = scanner.nextInt();
+        }
+
+        System.out.println(main.solution(graph, v));
     }
 }
